001f3c7b28a27e36bde5147b2e4ab4bd3089a503
ilisiecki <igor.fox.lisiecki@gmail.com>
2023-04-24 15:54:56 +0100

    Update header and theme button

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import ThemeButton from "./themeButton";
+import { Logo } from "./icons/logo";
+import Link from "next/link";
 
 export const Header = () => {
   const [mounted, setMounted] = useState(false);
@@ -11,9 +13,16 @@ export const Header = () => {
   return (
     <header className="flex h-24 w-full items-center justify-center border-[1px] border-red-500">
       <div className="mx-4 flex w-full max-w-screen-2xl items-center justify-between border-[1px] border-blue-500">
-        <div className="flex">
-          LOGO
-          <div>PAGES</div>
+        <div className="flex items-center">
+          <Logo />
+          <div className="ml-8 flex gap-2">
+            <div>
+              <Link href="/">Home</Link>
+            </div>
+            <div>
+              <Link href="/history">History</Link>
+            </div>
+          </div>
         </div>
         <div> {mounted && <ThemeButton />}</div>
       </div>
--- src/pages/components/themeButton.tsx
+++ src/pages/components/themeButton.tsx
@@ -1,5 +1,7 @@
 import { useTheme } from "next-themes";
 import { memo } from "react";
+import { Sun } from "./icons/sun";
+import { Moon } from "./icons/moon";
 
 const ThemeButton = () => {
   const { systemTheme, theme, setTheme } = useTheme();
@@ -10,18 +12,18 @@ const ThemeButton = () => {
       {currentTheme === "dark" ? (
         <button
           onClick={() => setTheme("light")}
-          className="flex h-10 w-10 items-center justify-center rounded-full bg-white text-neutral-900"
+          className="flex items-center justify-center"
           aria-label="Toggle light mode"
         >
-          O
+          <Sun />
         </button>
       ) : (
         <button
           onClick={() => setTheme("dark")}
-          className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-900 text-white"
+          className="flex items-center justify-center rounded-full"
           aria-label="Toggle dark mode"
         >
-          X
+          <Moon />
         </button>
       )}
     </div>
